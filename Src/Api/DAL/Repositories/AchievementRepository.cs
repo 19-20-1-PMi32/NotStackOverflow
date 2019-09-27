11f80f341782f067da0939e88d44849437a29aa3
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-09-27 17:23:50 +0300

    Add generic repository
    Add skeleton for repository pattern:
    1) generic repository
    2) interfaces and repositories for all entities

---
+++ Src/Api/DAL/Repositories/AchievementRepository.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    public class AchievementRepository: Repository<Achievement>, IAchievementsRepository
+    {
+        public AchievementRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+    }
+}
