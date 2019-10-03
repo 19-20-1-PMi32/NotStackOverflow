7c4686e7efd25a14fa583e210e60d5e8950c6ac7
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-03 21:33:13 +0300

    add jwt generator method

---
+++ Src/Api/Api/JwtGenerator/JwtGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Auth;
+using BLL;
+using BLL.Interfaces;
+using BLL.Service;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace Api.JwtGenerator
+{
+    public class JwtGenerator
+    {
+        private readonly IUserService _userService;
+        private readonly IAccountService _accountService;
+
+        public JwtGenerator(IUserService userService, IAccountService accountService)
+        {
+            _userService = userService;
+            _accountService = accountService;
+        }
+
+        public JwtGenerator(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public string GetToken(string username, string password)
+        {
+            
+            var user = _userService.GetUserByEmailAndPass(username, password);
+            var identity = _accountService.GetIdentity(user.Email, user.Role);
+            if (identity == null)
+            {
+                return "Invalid username or password.";
+            }
+
+            var now = DateTime.UtcNow;
+
+
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                notBefore: now,
+                claims: identity.Claims,
+                expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+           
+           
+            return JsonConvert.SerializeObject(response, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            });
+        }
+    }
+}
