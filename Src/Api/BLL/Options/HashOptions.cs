e10208e5ad3e7bc0004bfffa73bf55800efda684
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-07 15:52:35 +0300

    Create HashOptions.cs

---
+++ Src/Api/BLL/Options/HashOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Options
+{
+    public class HashOptions
+    {
+        public int Iterations { get; set; } = 10000;
+    }
+}
