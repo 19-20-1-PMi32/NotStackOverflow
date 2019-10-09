9f8c81b01b0cfe9f41be04446ad15662de17bdf7
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-09 14:20:58 +0300

    add files

---
+++ Src/Api/BLL/AuthOptions/JWTValidationParameters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BLL.AuthOptions
+{
+    public class JWTValidationParameters
+    {
+        public static TokenValidationParameters TokenValidationParameters = new TokenValidationParameters
+        {
+
+            ValidateIssuer = true,
+
+            ValidIssuer = AuthOptions.ISSUER,
+
+            ValidateAudience = true,
+
+            ValidAudience = AuthOptions.AUDIENCE,
+
+            ValidateLifetime = true,
+
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+
+            ValidateIssuerSigningKey = true,
+        };
+
+        public static TokenValidationParameters ExpiredTokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+
+            ValidateIssuer = false,
+
+            ValidateIssuerSigningKey = true,
+
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+
+            ValidateLifetime = false
+        };
+    };
+}
