c1583c75e8edbae508ccafcaa75138e08ca7b9ca
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-09 14:21:18 +0300

    code refactoring

--- Src/Api/Api/Startup.cs
+++ Src/Api/Api/Startup.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Api.Auth;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using BLL;
+using BLL.AuthOptions;
 using BLL.Module;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Api
 {
@@ -40,27 +31,10 @@ namespace Api
                 .AddJwtBearer(options =>
                 {
                     options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-
-                        ValidateIssuer = true,
-
-                        ValidIssuer = AuthOptions.ISSUER,
-
-                        ValidateAudience = true,
-
-                        ValidAudience = AuthOptions.AUDIENCE,
-
-                        ValidateLifetime = true,
-
-                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-
-                        ValidateIssuerSigningKey = true,
-                    };
+                    options.TokenValidationParameters = JWTValidationParameters.TokenValidationParameters;
                 });
 
-           
-
+            
             var builder = new ContainerBuilder();
             builder.RegisterModule(new BLLModule());
             builder.Populate(services);
