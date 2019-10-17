e16f184e6083f8abdfafd4b088a006be913c1f8c
Ostap Kutyanskyy <ostapkutya@gmail.com>
2019-10-17 13:31:07 +0300

    add authorize users table to db

---
+++ Src/Api/DAL/Migrations/20191016170825_addAutorizedUserTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddAuthorizedUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.CreateTable(
+                name: "AuthorizedUsers",
+                columns: table => new
+                {
+                    Email = table.Column<string>(nullable: false),
+                    RefreshToken = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorizedUsers", x => x.Email);
+                    table.UniqueConstraint("AK_AuthorizedUsers_RefreshToken", x => x.RefreshToken);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorizedUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ Src/Api/DAL/Repositories/AuthorizedUsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.Context;
+using DAL.Entities;
+using DAL.Interfaces;
+
+namespace DAL.Repositories
+{
+    public class AuthorizedUsersRepository: Repository<AuthorizedUser>, IAuthorizedUsersRepository
+    {
+        public AuthorizedUsersRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        public AuthorizedUser GetAuthorizedUser(string email)
+        {
+            return dbContext.AuthorizedUsers.FirstOrDefault(e => e.Email == email);
+        }
+    }
+}
