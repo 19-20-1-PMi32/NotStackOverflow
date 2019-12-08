d62267044aed59fc889bcf7f7aae9ce069fa799a
Ostapcs <ostapkutya@gmail.com>
2019-12-08 20:13:06 +0200

    add tag service

---
+++ Src/Api/BLL/Service/TagService.cs
@@ -0,0 +1,18 @@
+using BLL.DTOEntities;
+using BLL.Interfaces;
+
+namespace BLL.Service
+{
+    public class TagService : ITagService
+    {
+        public void Create(TagDTO tagDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
