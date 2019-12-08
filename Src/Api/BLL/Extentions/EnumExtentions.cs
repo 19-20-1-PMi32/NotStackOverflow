6d2c4cdfa46a1994d5a44f5f5dc06263f3f903e2
Taras Vysapinskyi <07032000T@gmail.com>
2019-12-08 23:55:31 +0200

    Added extention

---
+++ Src/Api/BLL/Extentions/EnumExtentions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BLL.Extentions
+{
+    public static class EnumExtentions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            if (field != null)
+            {
+                var stringAttribute = field.GetCustomAttribute<DescriptionAttribute>();
+                if (stringAttribute != null)
+                {
+                    return stringAttribute.Description;
+                }
+
+                return field.Name;
+            }
+
+            return null;
+        }
+    }
+}
