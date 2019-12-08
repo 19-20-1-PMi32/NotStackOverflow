def028f904afdc2e76f5fa6f8b030e4bbeb629c2
Ostapcs <ostapkutya@gmail.com>
2019-12-08 22:09:59 +0200

    add tag controller

---
+++ Src/Api/Api/Controllers/TagController.cs
@@ -0,0 +1,20 @@
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ITagService _tagService;
+
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+        
+        
+    }
+
+}
