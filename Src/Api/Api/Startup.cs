f4fa1dca59f1b836f2e09f63847a3e15fb0b4d23
Ostapcs <ostapkutya@gmail.com>
2019-12-08 21:04:29 +0200

    add cors

--- Src/Api/Api/Startup.cs
+++ Src/Api/Api/Startup.cs
@@ -55,7 +55,9 @@ namespace Api
                 app.UseHsts();
             }
 
-            app.UseCors(builder => builder.AllowAnyOrigin());
+            app.UseCors(builder => builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
             
             app.UseHttpsRedirection();
             app.UseDefaultFiles();
