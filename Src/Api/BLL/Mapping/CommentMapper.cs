e965cbc2a40647327e952847b6f6acbf38344f74
Ostapcs <ostapkutya@gmail.com>
2019-12-09 22:43:55 +0200

    fix comment mapper

--- Src/Api/BLL/Mapping/CommentMapper.cs
+++ Src/Api/BLL/Mapping/CommentMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using BLL.DTOEntities;
 using DAL.Entities;
@@ -11,18 +12,38 @@ namespace BLL.Mapping
         public static ICollection<CommentDTO> ToCommentDTOs(this IEnumerable<Comment> comments)
         {
             var commentDTOs = new List<CommentDTO>();
-
             foreach (var comment in comments)
-            {
-                commentDTOs.Add(new CommentDTO()
                 {
-                    Id = comment.Id,
-                    PostId = comment.PostId,
-                    PosNum = comment.PosNum,
-                    Text = comment.Text,
-                    //User = comment.User
-                });
-            }
+                    if (comment.User == null)
+                    {
+                        commentDTOs.Add(new CommentDTO()
+                        {
+                            Id = comment.Id,
+                            PostId = comment.PostId,
+                            PosNum = comment.PosNum,
+                            Text = comment.Text,
+                            //User = user
+                        });
+                        break;
+                    }
+
+                    var user = new UserDTO() 
+                    {
+                        Id = comment.UserId,
+                        Name = comment.User.Name,
+                        Surname = comment.User.Surname,
+                        NickName = comment.User.NickName,
+                    };
+                    
+                    commentDTOs.Add(new CommentDTO()
+                    {
+                        Id = comment.Id,
+                        PostId = comment.PostId,
+                        PosNum = comment.PosNum,
+                        Text = comment.Text,
+                        User = user
+                    });
+                }
 
             return commentDTOs;
         }
