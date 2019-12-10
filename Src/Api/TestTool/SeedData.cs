b2e5ccbe7a66e52ed29a29884ba756227f6bc3a4
Ogirok13 <34402913+Ogirok13@users.noreply.github.com>
2019-12-11 01:02:16 +0200

    Added records for tags, comments and included them to posts. And some other small changes

--- Src/Api/TestTool/SeedData.cs
+++ Src/Api/TestTool/SeedData.cs
@@ -1,12 +1,8 @@
-﻿using Autofac;
-using BLL.DTOEntities;
-using BLL.Interfaces;
-using BLL.Module;
-using DAL.Interfaces;
+﻿using DAL.UnitOfWork;
+using DAL.Context;
+using DAL.Entities;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
 
 namespace TestTool
 {
@@ -15,28 +11,24 @@ namespace TestTool
     {
         public static void Reset()
         {
-            using (var container = ConfigurerBLL.ConfigureDependencies())
-            {
-                var uof = container.Resolve<IUnitOfWork>();
-                var userService = container.Resolve<IUserService>();
-                var postService = container.Resolve<IPostService>();
+            var uof = new UnitOfWork(new ApplicationContext());
 
 
-                foreach (var achievment in uof.Achievements.GetAll()) uof.Achievements.Remove(achievment);
-                foreach (var authorizedUser in uof.AuthorizedUsers.GetAll()) uof.AuthorizedUsers.Remove(authorizedUser);
-                foreach (var comment in uof.Comments.GetAll()) uof.Comments.Remove(comment);
-                foreach (var post in uof.Posts.GetAll()) uof.Posts.Remove(post);
-                foreach (var postTag in uof.PostTags.GetAll()) uof.PostTags.Remove(postTag);
-                foreach (var tag in uof.Tags.GetAll()) uof.Tags.Remove(tag);
-                foreach (var userAchievment in uof.UserAchievements.GetAll()) uof.UserAchievements.Remove(userAchievment);
-                foreach (var user in uof.Users.GetAll()) uof.Users.Remove(user);
-                foreach (var vote in uof.Votes.GetAll()) uof.Votes.Remove(vote);
-                uof.Save();
+            foreach (var achievment in uof.Achievements.GetAll()) uof.Achievements.Remove(achievment);
+            foreach (var authorizedUser in uof.AuthorizedUsers.GetAll()) uof.AuthorizedUsers.Remove(authorizedUser);
+            foreach (var comment in uof.Comments.GetAll()) uof.Comments.Remove(comment);
+            foreach (var post in uof.Posts.GetAll()) uof.Posts.Remove(post);
+            foreach (var postTag in uof.PostTags.GetAll()) uof.PostTags.Remove(postTag);
+            foreach (var tag in uof.Tags.GetAll()) uof.Tags.Remove(tag);
+            foreach (var userAchievment in uof.UserAchievements.GetAll()) uof.UserAchievements.Remove(userAchievment);
+            foreach (var user in uof.Users.GetAll()) uof.Users.Remove(user);
+            foreach (var vote in uof.Votes.GetAll()) uof.Votes.Remove(vote);
+            uof.Save();
 
 
-                List<CreateUserDTO> usersData = new List<CreateUserDTO>()
+            List<User> usersData = new List<User>()
                 {
-                    new CreateUserDTO()
+                    new User()
                     {
                         Name = "Vitaliy",
                         Surname = "Petrenko",
@@ -44,7 +36,7 @@ namespace TestTool
                         Role = "User",
                         Email = "vpetrenko@gmail.com"
                     },
-                    new CreateUserDTO()
+                    new User()
                     {
                         Name = "Bohdan",
                         Surname = "Polishchuk",
@@ -52,7 +44,7 @@ namespace TestTool
                         Role = "Admin",
                         Email = "bpolishchuk@gmail.com"
                     },
-                    new CreateUserDTO()
+                    new User()
                     {
                         Name = "Maks",
                         Surname = "Kurnosenko",
@@ -61,38 +53,124 @@ namespace TestTool
                         Email = "kmaksim@gmail.com"
                     }
                 };
-                foreach (var user in usersData) userService.CreateUser(user);
+            foreach (var user in usersData) uof.Users.Add(user);
+            uof.Save();
 
-                List<CreatePostDTO> postsData = new List<CreatePostDTO>()
+
+            List<Post> postsData = new List<Post>()
                 {
-                    new CreatePostDTO()
+                    new Post()
                     {
                         Text = "Test post created by Vitya",
                         DateOfPublish = DateTime.Now,
                         UserId = uof.Users.GetUserByEmail("vpetrenko@gmail.com").Id
                     },
-                    new CreatePostDTO()
+                    new Post()
                     {
                         Text = "Created by Bodya",
                         DateOfPublish = DateTime.Now,
                         UserId = uof.Users.GetUserByEmail("bpolishchuk@gmail.com").Id
                     },
-                    new CreatePostDTO()
+                    new Post()
                     {
                         Text = "First post created by Maks",
                         DateOfPublish = DateTime.Now,
                         UserId = uof.Users.GetUserByEmail("kmaksim@gmail.com").Id
                     },
-                    new CreatePostDTO()
+                    new Post()
                     {
                         Text = "Second post created by Maks",
                         DateOfPublish = DateTime.Now,
                         UserId = uof.Users.GetUserByEmail("kmaksim@gmail.com").Id
                     }
                 };
-                foreach (var post in postsData) postService.CreatePost(post);
+            foreach (var post in postsData) uof.Posts.Add(post);
+            uof.Save();
 
-            }
+
+            List<int> postIds = new List<int>();
+            foreach (var post in uof.Posts.GetAll()) postIds.Add(post.Id);
+
+
+            List<Comment> commentData = new List<Comment>()
+                {
+                    new Comment()
+                    {
+                        DateOfPublish = DateTime.Now,
+                        Text = "Good post",
+                        UserId = uof.Users.GetUserByEmail("kmaksim@gmail.com").Id,
+                        PostId = postIds[0]
+                    },
+                    new Comment()
+                    {
+                        DateOfPublish = DateTime.Now,
+                        Text = "Very good post",
+                        UserId = uof.Users.GetUserByEmail("bpolishchuk@gmail.com").Id,
+                        PostId = postIds[0]
+                    },
+                    new Comment()
+                    {
+                        DateOfPublish = DateTime.Now,
+                        Text = "Bad post",
+                        UserId = uof.Users.GetUserByEmail("bpolishchuk@gmail.com").Id,
+                        PostId = postIds[1]
+                    }
+                };
+            foreach (var comment in commentData) uof.Comments.Add(comment);
+            uof.Save();
+
+
+            List<Tag> tagData = new List<Tag>()
+            {
+                new Tag()
+                {
+                    Title = "C++",
+                    Description = "This post includes C++"
+                },
+                new Tag()
+                {
+                    Title = "C#",
+                    Description = "This post includes C#"
+                },
+                new Tag()
+                {
+                    Title = "Python",
+                    Description = "This post includes Python"
+                },
+                new Tag()
+                {
+                    Title = "Java",
+                    Description = "This post includes Jaba"
+                }
+            };
+            foreach (var tag in tagData) uof.Tags.Add(tag);
+            uof.Save();
+
+
+            List<int> tagIds = new List<int>();
+            foreach (var tag in uof.Tags.GetAll()) tagIds.Add(tag.Id);
+
+
+            List<PostTags> postTagsData = new List<PostTags>()
+            {
+                new PostTags()
+                {
+                    PostId = postIds[0],
+                    TagId = tagIds[1]
+                },
+                new PostTags()
+                {
+                    PostId = postIds[0],
+                    TagId = tagIds[0]
+                },
+                new PostTags()
+                {
+                    PostId = postIds[1],
+                    TagId = tagIds[3]
+                }
+            };
+            foreach (var postTag in postTagsData) uof.PostTags.Add(postTag);
+            uof.Save();
         }
     }
 }
