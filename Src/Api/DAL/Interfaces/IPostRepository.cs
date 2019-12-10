ee7987c005330fe2379e0c154379e1831b57ad3e
Ostapcs <ostapkutya@gmail.com>
2019-12-10 22:45:43 +0200

    add new mehods

--- Src/Api/DAL/Interfaces/IPostRepository.cs
+++ Src/Api/DAL/Interfaces/IPostRepository.cs
@@ -7,11 +7,16 @@ namespace DAL.Interfaces
 {
     public interface IPostRepository : IRepository<Post>
     {
-        IEnumerable<Post> GetPostsWithComments(int postId, int page);
+        IEnumerable<Post> GetPostsWithComments(int postId);
 
         IEnumerable<Post> GetPostList(int amount);
 
         IEnumerable<Post> GetUsersPostsById(int userId);
 
+        IEnumerable<Post> OrderByLike(int page);
+        IEnumerable<Post> OrderByDislike(int page);
+        IEnumerable<Post> OrderByDate(int page);
+
+        int GetPostCount();
     }
 }
